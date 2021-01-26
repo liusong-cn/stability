7ee3d6f9bee33dae2d0723c26bb14acef8e542c6
liusong <1141185877@qq.com>
2021-01-26 17:30:29 +0800

    初始化

---
+++ src/main/java/com/bzzn/stability/StabilityApplication.java
@@ -0,0 +1,13 @@
+package com.bzzn.stability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StabilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bzzn/stability/StabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bzzn.stability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
